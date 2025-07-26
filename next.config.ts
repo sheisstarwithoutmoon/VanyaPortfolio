bd332b8b17b89a4e05978bbfbcbeb90b4e000e7b
Vanya <iamvanyaawasthi@gmail.com>
2025-07-27 02:51:21 +0530

    Final commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import Image from 'next/image';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'export', 
 };
 
 export default nextConfig;
