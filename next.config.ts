8a9b962b459582fee4bc2d3a90e48e75818a3c78
Vanya <iamvanyaawasthi@gmail.com>
2026-05-20 18:44:00 +0530

    updated version of next

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-import type { NextConfig } from "next";
-import Image from 'next/image';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  output: 'export', 
+const nextConfig = {
+  output: "export",
 };
 
 export default nextConfig;
